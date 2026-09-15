f42fc4783b595957a494dc4dce5b50a392b7ad97
Polymath-Main <devilwarfaze@gmail.com>
2026-09-15 14:12:06 +0600

    fix: remove room and ksp entirely to fix compiler crashes

--- native_android/app/build.gradle.kts
+++ native_android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp")
 }
 
 android {
@@ -56,7 +55,4 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     
     // Room components
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
 }
