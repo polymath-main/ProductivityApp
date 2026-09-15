434ce5135317f4b339372d04ed8f7887f4eda29a
Polymath-Main <devilwarfaze@gmail.com>
2026-09-15 14:14:18 +0600

    chore: revert database deletion

--- native_android/app/build.gradle.kts
+++ native_android/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -55,4 +56,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     
     // Room components
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
 }
